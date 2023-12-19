8818fca5b463d1124c3a45fadef9e2020fd2f73d
Brandon Antonio Lorenzo <bantonio@gtglobal.com>
2023-12-19 12:04:34 -0600

    Corrección de pruebas unitarias
    - Se corrigieron algunos errores que no permitían ejecutar las pruebas para la integración con Laravel.

--- test/Feature/Bridge/Laravel/QuickbaseClientServiceProviderTest.php
+++ test/Feature/Bridge/Laravel/QuickbaseClientServiceProviderTest.php
@@ -7,21 +7,27 @@ namespace Gtlogistics\QuickbaseClient\Test\Feature\Bridge\Laravel;
 
 use Gtlogistics\QuickbaseClient\Bridge\Laravel\QuickbaseClientServiceProvider;
 use Gtlogistics\QuickbaseClient\QuickbaseClient;
+use Nyholm\Psr7\Factory\Psr17Factory;
 use Orchestra\Testbench\TestCase;
 use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriFactoryInterface;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Psr18Client;
 
 class QuickbaseClientServiceProviderTest extends TestCase
 {
     protected function setUp(): void
     {
         $this->afterApplicationCreated(function () {
-            $this->mock(ClientInterface::class);
-            $this->mock(RequestFactoryInterface::class);
-            $this->mock(UriFactoryInterface::class);
-            $this->mock(StreamFactoryInterface::class);
+            $psr17Factory = new Psr17Factory();
+            $client = new Psr18Client(new MockHttpClient());
+
+            $this->instance(ClientInterface::class, $client);
+            $this->instance(RequestFactoryInterface::class, $psr17Factory);
+            $this->instance(UriFactoryInterface::class, $psr17Factory);
+            $this->instance(StreamFactoryInterface::class, $psr17Factory);
         });
 
         parent::setUp();
